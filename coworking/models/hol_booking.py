795196f3249ae348d6982a1420412371d96a04e1
Kartika Yudha Ferdiansyah <k.yudha.f@gmail.com>
2025-03-09 10:27:30 +0800

    Update hol_booking.py
    dirapikan

--- coworking/models/hol_booking.py
+++ coworking/models/hol_booking.py
@@ -4,21 +4,9 @@ class HolBooking(models.Model):
     _name = 'coworking.hol_booking'
     _description = 'Coworking Space Booking'
 
-    coworking_hol_space_id = fields.Many2one(
-        'coworking.hol_space', 
-        string='Floor Plan', 
-        required=True
-    )
-    hol_member_id = fields.Many2one(
-        'coworking.hol_member', 
-        string='Membership', 
-        required=True
-    )
-    hol_booking_date = fields.Datetime(
-        string='Booking Date', 
-        default=fields.Datetime.now, 
-        required=True
-    )
+    coworking_hol_space_id = fields.Many2one('coworking.hol_space', string='Floor Plan', required=True)
+    hol_member_id = fields.Many2one('coworking.hol_member', string='Membership', required=True)
+    hol_booking_date = fields.Datetime(string='Booking Date', default=fields.Datetime.now, required=True)
     state = fields.Selection([
         ('draft', 'Draft'),
         ('confirmed', 'Confirmed'),
